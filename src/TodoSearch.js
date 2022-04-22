7bc81bca4b227b9c1fb1ae6e1c2c197a2bca5375
Fernando Cueto <v-fcueto@youngliving.com>
2022-04-22 16:49:13 -0500

    add events to components

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,8 +2,16 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+    const onSearchValuechange = (event) => {
+        console.log(event.target.value);
+    }
+
     return (
-        <input className="TodoSearch" placeholder="Buscar To Do" />
+        <input 
+            className="TodoSearch" 
+            placeholder="Buscar To Do" 
+            onChange={onSearchValuechange}
+        />
     )
 }
 
