d0992d23e3add70510d2a93a3d0258a23078ed1d
Fernando Cueto <fernandoecueto@gmail.com>
2022-04-30 13:31:44 -0500

    implement context with React.useContext

--- src/components/App/AppUI.js
+++ src/components/App/AppUI.js
@@ -7,31 +7,29 @@ import { CreateTodoButton } from '../CreateTodoButton';
 import { TodoContext } from '../TodoContext';
 
 function AppUI() {
+	const {error, loading, searchedTodos, completeTodo, uncompleteTodo, deleteTodo} = React.useContext(TodoContext);
+
 	return (
 		<React.Fragment>
 			<TodoCounter/>
 
 			<TodoSearch/>
 
-			<TodoContext.Consumer>
-				{({error, loading, searchedTodos, completeTodo, uncompleteTodo, deleteTodo}) => (
-					<TodoList>
-						{error && <p>Hubo un error</p>}
-						{loading && <p>Cargando informacion...</p>}
-						{(!loading && !searchedTodos.length) && <p>Crear el primer todo</p>}
+			<TodoList>
+				{error && <p>Hubo un error</p>}
+				{loading && <p>Cargando informacion...</p>}
+				{(!loading && !searchedTodos.length) && <p>Crear el primer todo</p>}
 
-						{searchedTodos.map(todo => (
-						<TodoItem 
-								key={todo.text} 
-								text={todo.text} 
-								completed={todo.completed}
-								onComplete={ () => completeTodo(todo.text) }
-								onUncomplete={ () => uncompleteTodo(todo.text) }
-								onDelete={() => deleteTodo(todo.text)}/>  
-						))}
-					</TodoList>
-				)}
-			</TodoContext.Consumer>
+				{searchedTodos.map(todo => (
+				<TodoItem 
+						key={todo.text} 
+						text={todo.text} 
+						completed={todo.completed}
+						onComplete={ () => completeTodo(todo.text) }
+						onUncomplete={ () => uncompleteTodo(todo.text) }
+						onDelete={() => deleteTodo(todo.text)}/>  
+				))}
+			</TodoList>
 
 			<CreateTodoButton />
 			
--- src/components/TodoCounter/index.js
+++ src/components/TodoCounter/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import { TodoContext } from '../TodoContext';
 import './TodoCounter.css';
 
 
-function TodoCounter({ total, completed }) {
+function TodoCounter() {
+    const { totalTodos, completedTodos } = React.useContext(TodoContext);
 
     return (
-        <h2 className="TodoCounter">Ut enim ad minim {completed} de {total} veniam</h2>
+        <h2 className="TodoCounter">Completados {completedTodos} de {totalTodos} Tareas</h2>
     );
 }
 
