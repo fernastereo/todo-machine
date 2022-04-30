3644f48f55c2dbe20e720cb3494c5300cad86822
Fernando Cueto <fernandoecueto@gmail.com>
2022-04-30 09:52:17 -0500

    arrange files and components

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,5 @@
 import React from 'react';
-import { TodoCounter } from './TodoCounter';
-import { TodoSearch } from './TodoSearch';
-import { TodoList } from './TodoList';
-import { TodoItem } from './TodoItem';
-import { CreateTodoButton } from './CreateTodoButton';
-// import './App.css';
+import { AppUI } from './AppUI';
 
 const defaultTodos = [
   { text: 'Lorem ipsum dolor sit amet', completed: true},
@@ -56,32 +51,16 @@ function App() {
   }
 
   return (
-    <React.Fragment>
-      <TodoCounter
-        total={totalTodos}
-        completed={completedTodos}
-      />
-
-      <TodoSearch
-        searchValue={searchValue}
-        setSearchValue={setSearchValue}
-      />
-      
-      <TodoList>
-        {searchedTodos.map(todo => (
-          <TodoItem 
-            key={todo.text} 
-            text={todo.text} 
-            completed={todo.completed}
-            onComplete={ () => completeTodo(todo.text) }
-            onUncomplete={ () => uncompleteTodo(todo.text) }
-            onDelete={() => deleteTodo(todo.text)}/>  
-        ))}
-      </TodoList>
-
-      <CreateTodoButton />
-      
-    </React.Fragment>
+    <AppUI 
+      totalTodos={totalTodos}
+      completedTodos={completedTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      uncompleteTodo={uncompleteTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
 
